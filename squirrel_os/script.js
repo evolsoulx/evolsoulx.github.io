f7bcb13c960f833a6ec9071c1acbf89d06ff1f49
Tommy Filliater <527274+evolsoulx@users.noreply.github.com>
2023-05-16 13:29:28 -0400

    squirrel controls

--- squirrel_os/script.js
+++ squirrel_os/script.js
@@ -1,76 +1,40 @@
-function runAnimation() {
-    const squirrel = document.querySelector('#squirrel');
-    squirrel.classList.add('animate-squirrel'); // Add the animation class to the squirrel element
-    
-    const delay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay; // Generate a random delay between minDelay and maxDelay (in seconds)
+const squirrel = document.getElementById('squirrel');
+const container = document.getElementById('squirrel-container');
+squirrel
+const speed = 5;
+let dx = 0;
 
-    setTimeout(() => {
-      squirrel.classList.remove('animate-squirrel'); // Remove the animation class from the squirrel element
-      const nextDelay = Math.floor(Math.random() * (maxDelay - minDelay + 1)) + minDelay; // Generate the delay for the next animation loop
-      setTimeout(runAnimation, nextDelay * 1000); // Run the animation loop again with the next delay
-    }, animationDuration * 1000); // Set a timeout to remove the animation class after the animation duration (in seconds)
+window.addEventListener('keydown', (event) => {
+  if(event.key === "ArrowRight" || event.key === "ArrowLeft") {
+    squirrel.style.animationPlayState = "running";
+    dx = event.key === "ArrowRight" ? speed : -speed;
+    if(event.key === "ArrowLeft") {
+      squirrel.classList.add("flip");
+    } else {
+      squirrel.classList.remove("flip");
+    }
   }
+});
 
-  const minDelay = 1; // The minimum delay for the animation loop (in seconds)
-  const maxDelay = 10; // The maximum delay for the animation loop (in seconds)
-  const animationDuration = 1; // The duration of the animation (in seconds)
+window.addEventListener('keyup', (event) => {
+  if(event.key === "ArrowRight" || event.key === "ArrowLeft") {
+    squirrel.style.animationPlayState = "paused";
+    dx = 0;
+  }
+});
 
-  runAnimation(); // Start the animation loop
-
-
-  // Get the squirrel element
-const squirrel = document.querySelector('#squirrel');
-
-// Get the initial position of the squirrel from local storage (if it exists)
-const storedSquirrelPosition = localStorage.getItem('squirrelPosition');
-if (storedSquirrelPosition) {
-  const { left, top } = JSON.parse(storedSquirrelPosition);
-  squirrel.style.left = `${left}px`;
-  squirrel.style.top = `${top}px`;
+function animate() {
+  const squirrelRect = squirrel.getBoundingClientRect();
+  const containerRect = container.getBoundingClientRect();
+  const nextX = squirrelRect.x + dx;
+  if (nextX > containerRect.x + 10 && nextX < containerRect.x + containerRect.width - squirrelRect.width - 10) {
+    squirrel.style.left = `${nextX}px`;
+  }
+  requestAnimationFrame(animate);
 }
 
-// Set up mouse event listeners
-let isDragging = false;
-let mouseOffset = { x: 0, y: 0 };
-squirrel.addEventListener('mousedown', (event) => {
-  isDragging = true;
-  const squirrelRect = squirrel.getBoundingClientRect();
-  mouseOffset.x = event.clientX - squirrelRect.left;
-  mouseOffset.y = event.clientY - squirrelRect.top;
-});
 
-document.addEventListener('mousemove', (event) => {
-  if (isDragging) {
-    const squirrelContainerRect = squirrel.parentElement.getBoundingClientRect();
-    const squirrelWidth = squirrel.offsetWidth;
-    const squirrelHeight = squirrel.offsetHeight;
-    let left = event.clientX - mouseOffset.x - squirrelContainerRect.left;
-    let top = event.clientY - mouseOffset.y - squirrelContainerRect.top;
-
-    // Clamp the squirrel position to within the container
-    if (left < 0) {
-      left = 0;
-    } else if (left + squirrelWidth > squirrelContainerRect.width) {
-      left = squirrelContainerRect.width - squirrelWidth;
-    }
-    if (top < 0) {
-      top = 0;
-    } else if (top + squirrelHeight > squirrelContainerRect.height) {
-      top = squirrelContainerRect.height - squirrelHeight;
-    }
-
-    squirrel.style.left = `${left}px`;
-    squirrel.style.top = `${top}px`;
-  }
-});
-
-document.addEventListener('mouseup', (event) => {
-  isDragging = false;
-  const squirrelPosition = { left: squirrel.offsetLeft, top: squirrel.offsetTop };
-  localStorage.setItem('squirrelPosition', JSON.stringify(squirrelPosition));
-});
-
-
+animate();
 
 //timer for taskbar.
 function updateTime() {
